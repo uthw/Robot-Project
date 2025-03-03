7691dc0afc77515c864fb0e4aaf838e30325cb51
uthw <u.wood@icloud.com>
2025-03-03 16:49:15 -0500

    rcs initialization

---
+++ exp3_rcs.cpp
@@ -0,0 +1,6 @@
+#include <FEHRCS.h>
+
+int main(void) {
+    RCS.InitializeTouchMenu("0410H5NPE"); // Run menu to select region (ABCDEFGH)
+    // after this we need to find the red kill switch for our region and pull it
+}
