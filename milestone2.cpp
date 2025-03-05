3cdca328fc05356b92246d6bd666bf1440c22a7e
uthw <u.wood@icloud.com>
2025-03-05 16:42:09 -0500

    add motor battery adjustments

--- milestone2.cpp
+++ milestone2.cpp
@@ -2,25 +2,28 @@
 #include <FEHBattery.h>
 #include <FEHLCD.h>
 #include <FEHMotor.h>
+#include <FEHRCS.h>
 #include <FEHSD.h>
 #include <FEHServo.h>
 #include <FEHUtility.h>
+#include <algorithm>
 #include <cmath>
+using namespace std;
 
 #define MAX_SPEED 20
 #define MIN_SPEED 10
 #define LOW_BATTERY 3.0
-
-
 // these conversions will need to account for battery lvl later
-#define COUNTS_IN_90_DEGREES 110 * 2
-#define COUNTS_IN_1_INCH 20 * (17 / 10.5)
-
-
+#define COUNTS_IN_90_DEGREES 210
+#define COUNTS_IN_1_INCH 20
+#define LEFT_MODIFIER 0 // Added to left motor speed. Set to 0 if it's going straight right now
 
 // Convert desired degrees to number of counts required to rotate said number of degrees
 #define DEGREES_TO_COUNTS(degrees) (int)(degrees * COUNTS_IN_90_DEGREES / 90)
 #define INCHES_TO_COUNTS(inches) (int)(inches * COUNTS_IN_1_INCH)
+
+// Motors have less power as battery decreases
+#define ACTUAL_PERCENTAGE_POWER(percent) min((int)((11.5 / Battery.Voltage()) * percent), 100)
 
 // These may be different
 DigitalEncoder rightencoder(FEHIO::P0_0);
@@ -28,13 +31,10 @@ DigitalEncoder leftencoder(FEHIO::P0_1);
 FEHMotor rightmotor(FEHMotor::Motor0, 9.0);
 FEHMotor leftmotor(FEHMotor::Motor1, 9.0);
 
-
-
 #define DARK_THRESHOLD 2.5
 #define IS_DARK(voltage) (voltage > DARK_THRESHOLD)
 
 AnalogInputPin lightSensor(FEHIO::P1_0);
-
 
 void turnRight(int percent, int degrees)
 {
@@ -49,7 +49,7 @@ void turnRight(int percent, int degrees)
 
     // Set motors to desired percent
     rightmotor.SetPercent(percent * -1);
-    leftmotor.SetPercent(percent);
+    leftmotor.SetPercent(percent + LEFT_MODIFIER);
 
     // Run motors until avg of left and right encoder equals counts
     while ((leftencoder.Counts() + rightencoder.Counts()) / 2.0 < counts)
@@ -59,7 +59,6 @@ void turnRight(int percent, int degrees)
     rightmotor.Stop();
     leftmotor.Stop();
 }
-
 
 void turnLeft(int percent, int degrees)
 {
@@ -73,7 +72,7 @@ void turnLeft(int percent, int degrees)
     leftencoder.ResetCounts();
 
     // Set motors to desired percent
-    rightmotor.SetPercent(percent);
+    rightmotor.SetPercent(percent + LEFT_MODIFIER);
     leftmotor.SetPercent(percent * -1);
 
     // Run motors until avg of left and right encoder equals counts
@@ -103,7 +102,7 @@ void goForward(int percent, int inches)
 
     // Set motors to desired percent
     rightmotor.SetPercent(percent);
-    leftmotor.SetPercent(percent);
+    leftmotor.SetPercent(percent + LEFT_MODIFIER);
 
     // Run motors until avg of left and right encoder equals counts
     while ((leftencoder.Counts() + rightencoder.Counts()) / 2.0 < counts)
